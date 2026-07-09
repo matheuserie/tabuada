5cc132ac951979b1513df58ad0ef8646b07a8faf
Matheus Eriedson Abduche Silva <matheuseriedson27@gmail.comr>
2026-07-09 20:48:00 -0300

    first commit

---
+++ tabuada.js
@@ -0,0 +1,13 @@
+
+let i = prompt("Informe o valor de um número")
+
+for(let a = 1; a <= 10; a++) {
+    let x = i*a 
+    alert ("o resultado da tabuada é" + i + "*" + a + "=" + x)
+    
+
+}
+
+
+
+
